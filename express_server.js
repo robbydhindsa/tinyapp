3cd9d1af9a641239c3b1ec74e8c5cc2166416886
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-01-24 18:46:56 -0800

    Added delete function via new POST route and added a form element for each shortURL

--- express_server.js
+++ express_server.js
@@ -66,3 +66,9 @@ app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[id];
   res.redirect(longURL);
 });
+
+app.post("/urls/:id/delete", (req, res) => {
+  let id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect("/urls");
+});
