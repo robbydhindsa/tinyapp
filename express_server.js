aec74ba2fc81e8172747c550b3310db6f3bc80af
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-01-25 23:50:00 -0800

    Created login page and GET /login endpoint

--- express_server.js
+++ express_server.js
@@ -110,6 +110,13 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/login", (req, res) => {
+  const templateVars = {
+    user: users[req.cookies.user_id]
+  };
+  res.render("login", templateVars);
+});
+
 app.post("/login", (req, res) => {
   // res.cookie('username', req.body.username);
   res.redirect("/urls");
